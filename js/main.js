7b6723150713cdf9382e419f3c349c93b068a504
caiwoods11 <caiwoods11@byui.edu>
2022-02-23 15:58:26 -0800

    edit minor deets/fixed

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 let date_updated = "2/23/2022"
-console.log("print")
-document.querySelector("#date_updated").textContent = "Last Updated: " + date_updated
+document.querySelector("#date_updated").textContent = "Last Updated: " + date_updated;
+
+let year = Date.now().getYear();
+
+document.querySelector(".year").textContent = year;
+
