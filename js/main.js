e4e6f175e2e658b1c773ac16365148f42b5b1352
caiwoods11 <caiwoods11@byui.edu>
2022-02-23 16:10:29 -0800

    edit colors

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 let date_updated = "2/23/2022"
 document.querySelector("#date_updated").textContent = "Last Updated: " + date_updated;
 
-let year = Date.now().getYear();
-
+let year = new Date().getFullYear()
+console.log(year)
 document.querySelector(".year").textContent = year;
 
