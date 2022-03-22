9c8ddbc2d3e8b2889b93d7c2063343b0d5829940
caiwoods11 <caiwoods11@byui.edu>
2022-03-22 10:25:10 -0700

    update

---
+++ weather/js/forecast.js
@@ -0,0 +1,44 @@
+
+// let current = {} // 
+let daily_forecast = [] // 5-day
+
+let daysArray = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+
+function forecast_output () {
+    console.log(daily_forecast);
+
+    let cd = new Date( daily_forecast[0].dt).getDay();
+
+    for (let i = 0; i < daily_forecast.length && i < 5 ; i++){
+        console.log(i)
+        let day = daily_forecast[i];
+
+        let data = document.querySelector("#data-"+i);
+        data.textContent = daysArray[(cd + i) % 7]
+
+        let temp = document.querySelector("#temp-"+i);
+        temp.textContent = Math.round(day.temp.day, 0);
+        let d = document.querySelector("#daily-description-"+i);
+        d.textContent = day.weather[0].description;
+        let iconurl = document.querySelector("#iconurl-"+i);
+        iconurl.src = "http://openweathermap.org/img/w/" +day.weather[0].icon+ ".png";
+    }
+}
+
+async function FetchForecast(){
+    let url = "http://api.openweathermap.org/data/2.5/onecall?id=524901&appid=6012b03278f12830c67408ce29eef0f5&units=imperial&lat=42.10497&lon=-111.88393&exclude=hourly"
+    await fetch(url)
+    .then((response) => {
+        return response.json();
+    })
+    .then((dailyJSON) => {
+        console.log(dailyJSON)
+    daily_forecast = dailyJSON.daily
+    //forecase = dailyJSON.forecast.weather
+    // console.log(forecast)
+    //     wdata = weatherJSON.data.results;
+    });
+    forecast_output();
+}
+FetchForecast();
