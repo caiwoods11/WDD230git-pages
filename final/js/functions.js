c9d0f8633327d6af3b97a76b47808d3a87c4971e
caiwoods11 <caiwoods11@byui.edu>
2022-04-05 14:49:12 -0700

    add to git pages

---
+++ final/js/functions.js
@@ -0,0 +1,24 @@
+// Collapsible function
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+// Hamburger Nav
+
+const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('.navigation')
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
